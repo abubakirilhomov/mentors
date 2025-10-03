1c95da3962756165d8fb0aba51a54472a7b1181d
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-10-03 11:48:06 +0500

    asdada

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
