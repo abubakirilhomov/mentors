6cf10f330fe64ffb40d730f5c6ffc09e1c32966c
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-10-30 12:03:48 +0500

    add manifest.webmanifest for full PWA + push support

--- vite.config.ts
+++ vite.config.ts
@@ -2,50 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      strategies: 'injectManifest', // 👈 используем твой SW
-      srcDir: 'src',               // путь до sw.js
-      filename: 'sw.js',           // итоговый файл в продакшне
-      injectManifest: {
-        swSrc: 'src/sw.js',        // откуда брать код
-      },
       registerType: 'autoUpdate',
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js',
+      injectManifest: {
+        swSrc: 'src/sw.js',
+      },
+      manifest: false, // 👈 говорим, что свой манифест лежит в public/
       includeAssets: [
-        'favicon.svg',
         'favicon.ico',
         'robots.txt',
         'apple-touch-icon.png',
+        'manifest.webmanifest', // 👈 явно добавляем сюда
       ],
-      manifest: {
-        name: 'Mentors Internship',
-        short_name: 'Mentors',
-        description: 'Mentors App for Internhub',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
     }),
   ],
   optimizeDeps: {
